0ea9a47ed7b4624c1db2599b34905584b6d0f4b9
brettp <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-03-03 17:53:05 +0000

    Pulled in the interface changes.
    git-svn-id: http://code.elgg.org/elgg/trunk@5257 36083f99-b078-4883-b0ff-0f9b5a30f544

--- index.php
+++ index.php
@@ -1,36 +1,41 @@
 <?php
-/**
- * Elgg index page for web-based applications
- *
- * @package Elgg
- * @subpackage Core
- * @author Curverider Ltd
- * @link http://elgg.org/
- */
 
-/**
- * Start the Elgg engine
- */
-define('externalpage',true);
-require_once(dirname(__FILE__) . "/engine/start.php");
-
-if (!trigger_plugin_hook('index', 'system', null, FALSE)) {
 	/**
-	 * Check to see if user is logged in, if not display login form
-	 **/
+	 * Elgg profile index
+	 * 
+	 * @package ElggProfile
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Curverider Ltd <info@elgg.com>
+	 * @copyright Curverider Ltd 2008-2010
+	 * @link http://elgg.com/
+	 */
 
-	if (isloggedin()) {
-		forward('pg/dashboard/');
-	}
+	// Get the Elgg engine
+		require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
 
-	//Load the front page
-	global $CONFIG;
-	$title = elgg_view_title(elgg_echo('content:latest'));
-	set_context('search');
-	$content = elgg_list_registered_entities(array('limit' => 10, 'full_view' => FALSE, 'allowed_types' => array('object','group')));
-	set_context('main');
-	global $autofeed;
-	$autofeed = FALSE;
-	$content = elgg_view_layout('two_column_left_sidebar', '', $title . $content, elgg_view("account/forms/login"));
-	page_draw(null, $content);
-}
+	// Get the username
+		$username = get_input('username');
+		
+		$body = "";
+		
+	// Try and get the user from the username and set the page body accordingly
+		if ($user = get_user_by_username($username)) {
+			
+			if ($user->isBanned() && !isadminloggedin()) {
+				forward(); exit;
+			}
+			$body = elgg_view_entity($user,true);
+			$title = $user->name;
+
+			$body = elgg_view_layout('widgets',$body);
+			
+		} else {
+			
+			$body = elgg_echo("profile:notfound");
+			$title = elgg_echo("profile");
+			
+		}
+
+		page_draw($title, $body);
+		
+?>
